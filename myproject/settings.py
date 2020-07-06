02a3e5e8b086ac62946a5531989e03e709877622
amainfosoft <amainfosoft@gmail.com>
2020-07-06 11:54:05 +0530

    Changes on index

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(0=ju@m*!hj!u8@w1443%t+%t!f4e#ajg55#%d-gil+$x@)k9o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
